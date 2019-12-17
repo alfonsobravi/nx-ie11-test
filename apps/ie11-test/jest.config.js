49f96e58e2e135b96964a5b853d7109b345b45dc
Alf <alfonso.bravi@whanautahi.com>
2019-12-17 14:11:43 +1300

    initial commit

---
+++ apps/ie11-test/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ie11-test',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/ie11-test'
+};
